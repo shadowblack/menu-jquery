a97b2548280766473b21a1e4bca2228669e709da
ninja.aoshi@gmail.com <ninja.aoshi@gmail.com@e947741a-fadb-931f-bcf1-1c3e57018e3a>
2011-01-12 21:35:23 +0000

    afinando código, corrección de problema con repetición de datos

--- menu/jquery-menu.js
+++ menu/jquery-menu.js
@@ -17,8 +17,7 @@
 		
 		var config = $.extend(true,config,config_default);		
 	
-		console.debug(config);
-			alert("adfad");
+		console.debug(config);			
 		// validando datos entrantes en imagenes
 		jQuery.each(config,function(i,obj){		
 			_type = typeof(obj.img);			
@@ -34,13 +33,14 @@
 
 		/*
 		* recorriendo areglo de imagenes, retorna un valor puro para el array y manipula el contador, aunque esta declarada en 0,  el contador comienza en  1
-		*/
-		
-		
+		*/	
+var demo = 0;		
 		return this.each(function(){
+		    
+			demo = demo +1;
+			alert(demo);
 			var cou_cli = 0;
-			recorrer = function(ini,lim,con){		
-				alert(ini+' '+lim);
+			var recorrer = function(ini,lim,con){						
 				if (ini < lim){				
 					++ini;				
 				} else {
@@ -53,11 +53,9 @@
 			var vQuery = jQuery(this);
 			var len_img_cli = config.click.img.length,
 				len_img_hob = config.hover.img.length;						
-				
-						
+								
 			jQuery(vQuery).click(function(){								
-				jQuery(this).attr("src",config.click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);	
-				alert(cou_cli);
+				jQuery(this).attr("src",config.click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);					
 			});	
 
 						
