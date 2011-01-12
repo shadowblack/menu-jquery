0eee186ed66d910c4eeebb8edaa52c50dac04e91
ninja.aoshi@gmail.com <ninja.aoshi@gmail.com@e947741a-fadb-931f-bcf1-1c3e57018e3a>
2011-01-12 20:57:28 +0000

    Configuración para cambio de imagenes del mismo evento click

--- trunk/menu/jquery-menu.js
+++ trunk/menu/jquery-menu.js
@@ -7,15 +7,18 @@
 
 
 	jQuery.fn.menueimg = function(config){
+	
 		var config_default = 	{	
-									click : {img:['a','f'], event : undefined},
+									click : {img:[], event : undefined},
 									hover :  {img:Array([],[]),event : undefined}									
 								};
 		var cou_cli = 0,
 			cou_hov = 0;
 		
-		var config = $.extend(config,config_default);		
-
+		var config = $.extend(true,config,config_default);		
+	
+		console.debug(config);
+			alert("adfad");
 		// validando datos entrantes en imagenes
 		jQuery.each(config,function(i,obj){		
 			_type = typeof(obj.img);			
@@ -25,28 +28,39 @@
 				} else {
 					$.error("Tipo de dato incorrecto en el argumento para la imagen, en la funcion menueimg");						
 				}							
-				console.debug(typeof(_type));
-				return true;
+				console.debug(typeof(_type));				
 			} 								
-		});			
-		
-		var len_img_cli = config.click.img.length,
-			len_img_hob = config.hover.img.length;
-		
-		Query(this).click(function(){
-			recorrer()
-			
-		});
-		
-		recorrer = function(ini,lim,cou){
-			if (ini <= lim)
-				cou_cli++;
-			else
-				cou_cli = 0;
-			return cou;
-		}
-	
+		});		
+
+		/*
+		* recorriendo areglo de imagenes, retorna un valor puro para el array y manipula el contador, aunque esta declarada en 0,  el contador comienza en  1
+		*/
 		
 		
+		return this.each(function(){
+			var cou_cli = 0;
+			recorrer = function(ini,lim,con){		
+				alert(ini+' '+lim);
+				if (ini < lim){				
+					++ini;				
+				} else {
+					ini = 1;
+				}			
+				eval(con + '=' + ini);
+				return ini-1;
+			}			
+					
+			var vQuery = jQuery(this);
+			var len_img_cli = config.click.img.length,
+				len_img_hob = config.hover.img.length;						
+				
+						
+			jQuery(vQuery).click(function(){								
+				jQuery(this).attr("src",config.click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);	
+				alert(cou_cli);
+			});	
+
+						
+		});				
 	};
 })( jQuery );
