21b1c0cd50f1845c18bc51920e3189a8a13eb0d5
ninja.aoshi@gmail.com <ninja.aoshi@gmail.com@e947741a-fadb-931f-bcf1-1c3e57018e3a>
2011-01-13 04:09:34 +0000

    Definición de conceptos de que manera va a estructurarse la función en jQuery. Optimización del código.

--- trunk/menu/jquery-menu.js
+++ trunk/menu/jquery-menu.js
@@ -6,59 +6,50 @@
 (function( $ ){
 
 
-	jQuery.fn.menueimg = function(config){
-	
-		var config_default = 	{	
-									click : {img:[], event : undefined},
-									hover :  {img:Array([],[]),event : undefined}									
-								};
+	jQuery.fn.menueimg = function(config){		
+		var config_default = 	[{	
+									click : {img:[], event : undefined, switch : undefined},
+									hover :  {img:Array([],[]),event : undefined, switch : undefined}									
+								}];
 		var cou_cli = 0,
 			cou_hov = 0;
 		
-		var config = $.extend(true,config,config_default);		
-	
-		console.debug(config);			
-		// validando datos entrantes en imagenes
-		jQuery.each(config,function(i,obj){		
-			_type = typeof(obj.img);			
-			if(_type != 'object'){				
-				if (_type === 'string'){																	
-					obj.img = [obj.img];						
-				} else {
-					$.error("Tipo de dato incorrecto en el argumento para la imagen, en la funcion menueimg");						
-				}							
-				console.debug(typeof(_type));				
-			} 								
-		});		
-
-		/*
-		* recorriendo areglo de imagenes, retorna un valor puro para el array y manipula el contador, aunque esta declarada en 0,  el contador comienza en  1
-		*/	
-var demo = 0;		
-		return this.each(function(){
-		    
-			demo = demo +1;
-			alert(demo);
-			var cou_cli = 0;
-			var recorrer = function(ini,lim,con){						
-				if (ini < lim){				
-					++ini;				
-				} else {
-					ini = 1;
-				}			
-				eval(con + '=' + ini);
-				return ini-1;
+		var config = $.extend(true,config,config_default),			
+		cou_cli = 0,
+		recorrer = function(ini,lim,con){						
+			if (ini < lim){				
+				++ini;				
+			} else {
+				ini = 1;
 			}			
-					
+			eval(con + '=' + ini);
+			return ini-1;
+		},
+		base = function(){
+		
+		};
+		
+		this.each(function(i){		    			
+				
+			// validando datos
+			jQuery.each(config[i],function(i,obj){		
+				var _type = typeof(obj.img);			
+				if(_type != 'object'){				
+					if (_type === 'string'){																	
+						obj.img = [obj.img];						
+					} else {
+						jQuery.error("Tipo de dato incorrecto en el argumento para la imagen, en la funcion menueimg");						
+					}															
+				} 								
+			});
+			
 			var vQuery = jQuery(this);
-			var len_img_cli = config.click.img.length,
-				len_img_hob = config.hover.img.length;						
+			var len_img_cli = config[i].click.img.length,
+				len_img_hob = config[i].hover.img.length;						
 								
 			jQuery(vQuery).click(function(){								
-				jQuery(this).attr("src",config.click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);					
-			});	
-
-						
+				jQuery(this).attr("src",config[i].click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);					
+			});		
 		});				
 	};
 })( jQuery );
