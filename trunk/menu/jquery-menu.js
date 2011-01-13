614b5e43adbeff4bdbe9c4995b0d8899d65e0065
ninja.aoshi@gmail.com <ninja.aoshi@gmail.com@e947741a-fadb-931f-bcf1-1c3e57018e3a>
2011-01-13 20:36:24 +0000

    Creación de suicheo aun sin funcionar.

--- trunk/menu/jquery-menu.js
+++ trunk/menu/jquery-menu.js
@@ -6,17 +6,27 @@
 (function( $ ){
 
 
-	jQuery.fn.menueimg = function(config){		
-		var config_default = 	[{	
+	jQuery.fn.menueimg = function(arr_con){		
+		/*var arr_con_default = 	[{	
 									click : {img:[], event : undefined, switch : undefined},
 									hover :  {img:Array([],[]),event : undefined, switch : undefined}									
-								}];
+								}];*/
+		
+		var arr_str_val = [];
+		for(var i = 0; i < arr_con.length; i++){
+			var str_val = "{click : {img:[], event : undefined, switch : undefined},hover :  {img:Array([],[]),event : undefined, switch : undefined}}";
+			arr_str_val.push(str_val);
+		}
+		eval("var arr_con_default = ["+arr_str_val.join(",")+"]");
+		
 		var cou_cli = 0,
 			cou_hov = 0;
 		
-		var config = $.extend(true,config,config_default),			
-		cou_cli = 0,
-		recorrer = function(ini,lim,con){						
+		var arr_con = $.extend(true,arr_con,arr_con_default);				
+		
+		var cou_cli = 0;
+		
+		var recorrer = function(ini,lim,con){						
 			if (ini < lim){				
 				++ini;				
 			} else {
@@ -24,15 +34,18 @@
 			}			
 			eval(con + '=' + ini);
 			return ini-1;
-		},
+		}
+		
+		var odl_ind = undefined,
 		base = function(){
 		
-		};
-		
-		this.each(function(i){		    			
+		};		
 				
+		
+		this.each(function(i){		    						
+			
 			// validando datos
-			jQuery.each(config[i],function(i,obj){		
+			jQuery.each(arr_con[i],function(i,obj){		
 				var _type = typeof(obj.img);			
 				if(_type != 'object'){				
 					if (_type === 'string'){																	
@@ -43,12 +56,31 @@
 				} 								
 			});
 			
-			var vQuery = jQuery(this);
-			var len_img_cli = config[i].click.img.length,
-				len_img_hob = config[i].hover.img.length;						
+			var vQuery = jQuery(this)/*,
+				ele_arr.push(jQuery(vQuery));*/	
+				
+			var len_img_cli = arr_con[i].click.img.length,
+				len_img_hob = arr_con[i].hover.img.length;						
 								
-			jQuery(vQuery).click(function(){								
-				jQuery(this).attr("src",config[i].click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);					
+			jQuery(vQuery).click(function(){	
+				
+				jQuery(this).attr("src",arr_con[i].click.img[recorrer(cou_cli,len_img_cli,'cou_cli')]);	
+				if (arr_con[i].click.switch != undefined){		
+					alert(odl_ind+" "+i);
+					if (odl_ind != i && odl_ind != undefined){
+						alert("demo");
+						var cha_ind = odl_ind; ///hange index
+									
+						//if (cha_ind != undefined){
+						cou_cli = 0;
+						alert(arr_con[cha_ind].click.img[arr_con[cha_ind].click.switch]);
+						alert(cha_ind);
+							jQuery(vQuery[cha_ind]).attr("src",arr_con[cha_ind].click.img[arr_con[cha_ind].click.switch]);
+						//}
+					}		
+					odl_ind = i;
+				} 
+				
 			});		
 		});				
 	};
